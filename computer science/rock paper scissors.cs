0044e05fc0d2c692dde9e49309e384dc3a6e2717
Mistium <92952823+Mistium@users.noreply.github.com>
2023-09-14 14:35:25 +0100

    Update rock paper scissors.cs

--- computer science/rock paper scissors.cs
+++ computer science/rock paper scissors.cs
@@ -1,47 +1,77 @@
+using System;
+
 bool game = true;
-string move;
-string comp;
+char move = 'n';
+char comp = 'n';
 string win;
 int wins = 0;
 int plays = 0;
 int choice = -1;
 string moves = "rps";
+Random random = new Random(); // Added a Random object for generating computer moves
 
-console.WriteLine("Welcome To Rock Paper Scissors, Type 'Exit' At Any Point To Close The Game");
+Console.WriteLine("Welcome To Rock Paper Scissors, Type 'x' At Any Point To Close The Game");
+
 difficulty:
-console.WriteLine("Choose a difficulty (Impossible or Normal)");
-string difficulty = console.ReadLine().ToLower();
-if (difficulty != "impossible" || difficulty != "normal") {
-  goto difficulty;
-}
-while game {
-  choosemove:
-  console.WriteLine("Rock Paper Or Scissors? (R/P/S)");
-  move = console.ReadLine().ToLower();
-  if (!moves.Contains(move)){
-    console.WriteLine("Invalid Move");
-    goto choosemove;
-  }
-  if (difficulty == "Impossible") {
-    if (move == "r"):
-      comp = "p";
-    if (move == "p"):
-      comp = "s";
-    if (move == "s"):
-      comp = "r";
-  } else {
-    choice  = rnd.Next(0, 3);
-    move = moves[choice]
-  }
-  console.WriteLine("You Played {0}", move);
-  console.WriteLine("The Computer Played {0}", comp);
-  plays += 1;
-  if ((move == "r" && comp == "s") || (move == "p" && comp == "r") || (move == "s" && comp == "r")) {
-    win = "Player";
-    wins += 1
-  } else {
-    win = "Computer";
-  }
+Console.WriteLine("Choose a difficulty (Impossible or Normal)");
+string difficulty = Console.ReadLine().ToLower();
+if (difficulty != "impossible" && difficulty != "normal") // Changed || to &&
+{
+    goto difficulty;
 }
 
-console.WriteLine("You Won {0} Times", wins)
+while (game)
+{
+choosemove:
+    Console.WriteLine("Rock Paper Or Scissors? (R/P/S)");
+    move = Console.ReadLine().ToLower()[0];
+
+    if (move == 'x') // Added the ability to exit the game
+    {
+        game = false;
+        break;
+    }
+
+    if (!moves.Contains(move))
+    {
+        Console.WriteLine("Invalid Move");
+        goto choosemove;
+    }
+
+    if (difficulty == "impossible")
+    {
+        if (move == 'r')
+            comp = 'p';
+        else if (move == 'p') // Changed if statements to else if
+            comp = 's';
+        else if (move == 's')
+            comp = 'r';
+    }
+    else
+    {
+        choice = random.Next(0, 3);
+        comp = moves[choice]; // Corrected Character.toString to .ToString()
+    }
+
+    Console.WriteLine("You Played {0}", move);
+    Console.WriteLine("The Computer Played {0}", comp);
+    plays += 1;
+
+    if ((move == 'r' && comp == 's') || (move == 'p' && comp == 'r') || (move == 's' && comp == 'p'))
+    {
+        win = "Player";
+        wins += 1;
+    }
+    else if (move == comp)
+    {
+        win = "Tie";
+    }
+    else
+    {
+        win = "Computer";
+    }
+
+    Console.WriteLine("Winner: {0}", win);
+}
+
+Console.WriteLine("You Won {0} Times", wins);
